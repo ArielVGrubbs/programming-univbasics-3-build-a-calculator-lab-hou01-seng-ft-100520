ed74af97718a49ed5e9c8ce7cdc65530e893d952
Ariel Grubbs <arielvictor1234@gmail.com>
2020-09-05 23:29:39 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,31 @@
 def addition(num1, num2)
-
+  num1 + num2 = sum
+  puts sum
+  sum
 end
 
 def subtraction(num1, num2)
-
+  num1 - num2 = difference
+  puts difference
+  difference
 end
 
 def division(num1, num2)
-
+  num1 / num2 = dividend
+  puts dividend
+  dividend
 end
 
 def multiplication(num1, num2)
-
+  num1 * num2 = product
+  puts product
+  product
 end
 
 def modulo(num1, num2)
-
+  
 end
 
 def square_root(num)
-
+  
 end
